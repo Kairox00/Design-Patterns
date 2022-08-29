08f8bc337d3104714283fdaf103f5a748f2fe0dc
Khaled Romeh <khaledmromeh@gmail.com>
2022-08-29 20:13:24 +0200

    Memento

--- src/Behavioral/Memento/Adder.java
+++ src/Behavioral/Memento/Adder.java
@@ -3,6 +3,11 @@ package Behavioral.Memento;
 public class Adder {
 
   private int result;
+  private Memento memento;
+
+  public Adder() {
+    this.memento = new Memento(result);
+  }
 
   public void add(int newNumber) {
     result += newNumber;
@@ -10,6 +15,14 @@ public class Adder {
 
   public int getResult() {
     return result;
+  }
+
+  public void save(){
+    memento.setState(result);
+  }
+
+  public void undo(){
+    result = memento.getState();
   }
 
 }
